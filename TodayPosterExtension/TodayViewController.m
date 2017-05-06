71224f1a7bcf016d13fe5712e99872f2f1f67c7c
Bryan Luby <bryanluby@users.noreply.github.com>
2017-05-06 15:58:46 -0500

    Wire up a button for testing

--- TodayPosterExtension/TodayViewController.m
+++ TodayPosterExtension/TodayViewController.m
@@ -9,18 +9,28 @@
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
 
+#import "LUBAPI.h"
+
 @interface TodayViewController () <NCWidgetProviding>
 
 @end
 
 @implementation TodayViewController
 
-- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult result))completionHandler {
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult result))completionHandler
+{
     // Update your data and prepare for a snapshot. Call completion handler when you are done
     // with NoData if nothing has changed or NewData if there is new data since the last
     // time we called you
     completionHandler(NCUpdateResultNoData);
 }
 
-@end
+- (IBAction)postButtonPressed:(NSButton *)sender
+{
+    [LUBAPI postToMicroDotBlogWithText:@"Testing out posting API"
+                              appToken:@""
+                            completion:^(BOOL success) {
+                            }];
+}
 
+@end
