295f5997c1aa7d8e710cbd3efc53ec09e8d972c6
Bryan Luby <bryanluby@users.noreply.github.com>
2017-05-06 14:52:54 -0500

    Add initial api file

---
+++ TodayPosterExtension/LUBAPI.h
@@ -0,0 +1,13 @@
+//
+//  LUBAPI.h
+//  TodayPoster
+//
+//  Created by Bryan Luby on 5/6/17.
+//  Copyright © 2017 Bryan Luby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LUBAPI : NSObject
+
+@end
---
+++ TodayPosterExtension/LUBAPI.m
@@ -0,0 +1,13 @@
+//
+//  LUBAPI.m
+//  TodayPoster
+//
+//  Created by Bryan Luby on 5/6/17.
+//  Copyright © 2017 Bryan Luby. All rights reserved.
+//
+
+#import "LUBAPI.h"
+
+@implementation LUBAPI
+
+@end
