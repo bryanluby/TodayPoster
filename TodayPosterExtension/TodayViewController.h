1d02f0a1fb3d5b25c6fbe7c20c063bca9637a281
Bryan Luby <bryanluby@users.noreply.github.com>
2017-05-06 13:57:16 -0500

    Add extension target

---
+++ TodayPosterExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayPosterExtension
+//
+//  Created by Bryan Luby on 5/6/17.
+//  Copyright © 2017 Bryan Luby. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TodayViewController : NSViewController
+
+@end
