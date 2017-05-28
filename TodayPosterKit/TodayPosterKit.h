fbb8988b93c97462796a02a13e517281df3cdaa5
Bryan Luby <bryanluby@users.noreply.github.com>
2017-05-28 10:16:49 -0500

    Add initial shared framework target

---
+++ TodayPosterKit/TodayPosterKit.h
@@ -0,0 +1,17 @@
+//
+//  TodayPosterKit.h
+//  TodayPosterKit
+//
+//  Created by Bryan Luby on 5/28/17.
+//  Copyright © 2017 Bryan Luby. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TodayPosterKit.
+FOUNDATION_EXPORT double TodayPosterKitVersionNumber;
+
+//! Project version string for TodayPosterKit.
+FOUNDATION_EXPORT const unsigned char TodayPosterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodayPosterKit/PublicHeader.h>
---
+++ TodayPosterKitTests/TodayPosterKitTests.m
@@ -0,0 +1,27 @@
+//
+//  TodayPosterKitTests.m
+//  TodayPosterKitTests
+//
+//  Created by Bryan Luby on 5/28/17.
+//  Copyright © 2017 Bryan Luby. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TodayPosterKitTests : XCTestCase
+
+@end
+
+@implementation TodayPosterKitTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
